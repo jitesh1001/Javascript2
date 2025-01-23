dc82c119a96e141b177994b4e93aba9257658a17
jitesh1001 <152077702+jitesh1001@users.noreply.github.com>
2025-01-23 18:33:18 +0000

    clone

---
+++ 01_basics/clone.js
@@ -0,0 +1,39 @@
+// iteration 
+// let rectangle = {
+//     length : 1,
+//     breadth : 2
+// }
+
+// let src  = {};
+// for(let key in rectangle){
+//     src[key] = rectangle[key];
+// }
+
+// console.log(src);
+
+// assign
+
+
+// let obj1 = {
+//     val1 : 1,
+//     val2 : 2
+// }
+
+// let obj2 = {
+//     val3 : 4
+
+// }
+
+// let dest = Object.assign({},obj1,obj2);
+// console.log(dest);
+
+
+// spread operator
+
+
+const obj = {
+    name : "john",
+    age : 18
+}
+const des = {...obj};
+console.log(des);
--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,83 @@
-console.log("hello world");
+// factory function
+
+// function createRectanle(len,bre){
+//     rectangle  = {
+//         length : len,
+//         breadth : bre,
+//         draw(){
+//             console.log("drawinh the rectangle");
+//         }
+//     }
+//     return rectangle;
+// }
+
+
+// let rectangle1 = createRectanle(4,5);
+// console.log(rectangle1);
+
+// console.log(rectangle1.constructor);
+
+
+let factoryrec = new Function('len','bre',`
+     rectangle  = {
+        length : len,
+         breadth : bre,
+        draw(){
+             console.log("drawinh the rectangle");
+    }
+ }
+    return rectangle;
+    `
+ )
+
+ let rec = factoryrec(3,4);
+ console.log(rec);
+
+
+
+
+// constructor function
+
+
+// function Rectangle(len,bre){
+//     console.log(typeof this);
+//     this.length = len,
+//     this.breadth = this.breadth,
+//     this.draw = function(){
+//         console.log("drawing the rectangle");
+//     }
+// }
+
+// let rectangl1 = new Rectangle(4,5);
+
+// // to add the property
+
+// rectangl1.color = "yellow";
+// console.log(rectangl1);
+// delete rectangl1.color;
+// console.log(rectangl1);
+
+
+
+
+
+
+
+// let createRectangle =  new Function('length','breadth',`
+//      this.length = length;
+//      this.breadth = breadth;
+//      this.draw = function(){
+//         consoe.log("drawing the rectangle");
+//      }`
+// )
+
+
+
+// let rectangle = new createRectangle(3,4);
+// console.log(rectangle);
+
+
+
+
+
+
